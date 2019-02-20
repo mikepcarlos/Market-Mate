b2f65cadffdb175c633bb74bba449389ac5f1946
mikepcarlos <michaelpcarlos@gmail.com>
2019-02-19 20:44:21 -0500

    app bakcground color/logo

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Home from './Components/Home'
 import { BrowserRouter, Route } from 'react-router-dom';
-// import './App.css';
 
 class App extends Component {
   render() {
--- server/client/src/Components/Home.js
+++ server/client/src/Components/Home.js
@@ -3,7 +3,18 @@ import React from 'react';
 const Home = () => {
   return(
     <div className="homepage">
-      home page
+      <div className="logo">
+        <img src="/logo.png" alt=""/>
+      </div>
+      <div className="title">
+        <h3>Market</h3>
+      </div>
+      <div className="intro">
+
+      </div>
+      <div className="start-options">
+
+      </div>
     </div>
   );
 };
