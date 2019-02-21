84276c56f136a0aae5b7e82ca3b2d8c986c08ddf
mikepcarlos <michaelpcarlos@gmail.com>
2019-02-20 19:22:20 -0500

    designed title font and homepage color scheme

--- server/client/src/Components/Home.js
+++ server/client/src/Components/Home.js
@@ -3,17 +3,20 @@ import React from 'react';
 const Home = () => {
   return(
     <div className="homepage">
-      <div className="logo">
-        <img src="/logo.png" alt=""/>
-      </div>
-      <div className="title">
-        <h3>Market</h3>
-      </div>
-      <div className="intro">
+      <span></span>
+      <div className="home-content">
+        <div className="title">
+          <h3>mark.et</h3>
+        </div>
+        <div className="logo">
 
+        </div>
       </div>
-      <div className="start-options">
-
+      <div className="home-buttons">
+        <div className="start-options">
+          <button type="button">New Session</button>
+          <button type="button">Current Session</button>
+        </div>
       </div>
     </div>
   );
