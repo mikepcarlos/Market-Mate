c86924d67942938bc95d530e261a158fdb606d3e
mikepcarlos <michaelpcarlos@gmail.com>
2019-03-02 13:50:32 -0500

    added link to new item form

--- server/client/src/Components/NewList.js
+++ server/client/src/Components/NewList.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NewList = () => {
 
   return(
     <div>
       New List
+      <div className="add item">
+        <Link to='/new_item'>
+          <button type="button">Add Item</button>
+        </Link>
+      </div>
     </div>
   )
 
