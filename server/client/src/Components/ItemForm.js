61ebbc182d0909e39e3556804b117c2b9169b3ed
mikepcarlos <michaelpcarlos@gmail.com>
2019-03-02 14:01:35 -0500

    created jsx form tags

--- server/client/src/Components/ItemForm.js
+++ server/client/src/Components/ItemForm.js
@@ -12,10 +12,32 @@ class ItemForm extends Component {
       price: ""
     }
   }
+
+  handleSubmit = () => {
+    console.log("test");
+  }
+
   render(){
     return(
-      <div>
-        item form
+      <div className="item-form-container">
+        <form onSubmit={this.handleSubmit} autoComplete="off" className="new-item-form">
+          <label>What is it?</label>
+          <input type="text" name="name" value=""/>
+          <label>How many?</label>
+          <input type="text" name="name" value=""/>
+          <label>How Important?</label>
+          <input type="text" name="name" value=""/>
+          <label>How Much $$?</label>
+          <input type="text" name="name" value=""/>
+          <label>OR</label>
+          <div className="camera-btn-container">
+            <button type="button" className="camera-btn"></button>
+          </div>
+          <br></br>
+          <div className="submit-item-container">
+            <button type="Submit" className="submit-tem-button">Submit</button>
+          </div>
+        </form>
       </div>
     )
   }
