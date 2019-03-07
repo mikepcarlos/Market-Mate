822684b52091c1c0c12dbeb59676d8e02afea3f9
mikepcarlos <michaelpcarlos@gmail.com>
2019-03-07 15:12:50 -0500

    handled form change events

--- server/client/src/Components/ItemForm.js
+++ server/client/src/Components/ItemForm.js
@@ -6,10 +6,12 @@ class ItemForm extends Component {
     super()
 
     this.state = {
-      title: "",
-      quantity: null,
-      priority: null,
-      price: null
+      item: {
+        title: "",
+        quantity: "",
+        priority: "",
+        price: ""
+      }
     }
   }
 
@@ -17,18 +19,33 @@ class ItemForm extends Component {
     console.log("test");
   }
 
+  handleNewItemChange = (e) => {
+    const safeNewItem = {
+      ...this.state.item,
+        [e.target.name]: e.target.value
+    }
+
+    this.setState({
+      item: safeNewItem
+    })
+  }
+
   render(){
     return(
       <div className="item-form-container">
         <form onSubmit={this.handleSubmit} autoComplete="off" className="new-item-form">
+          <br></br>
           <label>What is it?</label>
-          <input type="text" name="name" value=""/>
+          <input onChange={this.handleNewItemChange} type="text" name="title" value={this.state.item.title}/>
+          <br></br>
           <label>How many?</label>
-          <input type="text" name="name" value=""/>
+          <input onChange={this.handleNewItemChange} type="text" name="quantity" value={this.state.item.quantity}/>
+          <br></br>
           <label>How Important?</label>
-          <input type="text" name="name" value=""/>
+          <input onChange={this.handleNewItemChange} type="text" name="priority" value={this.state.item.priority}/>
+          <br></br>
           <label>How Much $$?</label>
-          <input type="text" name="name" value=""/>
+          <input onChange={this.handleNewItemChange} type="text" name="price" value={this.state.item.price}/>
           <br></br>
           <div className="submit-item-container">
             <button type="Submit" className="submit-tem-button">Submit</button>
